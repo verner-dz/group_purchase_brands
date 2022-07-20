6918ef0d2a7c3f4f12783eccdec14007d755dac4
Verner Dsouza <verner.dz@gmail.com>
2022-07-20 15:59:55 -0400

    add seed

---
+++ src/seed.js
@@ -0,0 +1,62 @@
+export const furnitureBrands = [
+  {
+    id: 1,
+    name: 'Joybird'
+  },
+
+  {
+    id: 2,
+    name: 'Restoration Hardware'
+  },
+
+  {
+    id: 3,
+    name: 'West Elm'
+  },
+
+  {
+    id: 4,
+    name: 'Article'
+  },
+
+  {
+    id: 5,
+    name: 'Floyd'
+  },
+
+  {
+    id: 6,
+    name: 'Burrow'
+  },
+
+  {
+    id: 7,
+    name: 'Outer'
+  },
+
+  {
+    id: 8,
+    name: 'Castlery'
+  },
+
+
+  {
+    id: 9,
+    name: 'Tuv'
+  },
+
+  {
+    id: 10,
+    name: 'RH'
+  },
+
+  {
+    id: 11,
+    name: 'Oka'
+  },
+
+  {
+    id: 12,
+    name: 'Arhaus'
+  }
+]
