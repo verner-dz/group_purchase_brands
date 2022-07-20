ff6e4a11f9c466e2cb890d5d5f88ec621b4a3081
Verner Dsouza <verner.dz@gmail.com>
2022-07-20 18:32:29 -0400

    rearrange components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import '../App.css';
-import Brands from './Brands'
+// import Brands from './Brands'
 import Purchase from './Purchase'
-import GroupPurchasesPanel from './GroupPurchasesPanel'
 
 function App() {
   return (
@@ -9,19 +8,7 @@ function App() {
       <div className="App">
         <h1 className={'m-5'}> Group Brand Purchase App </h1>
 
-        <div className={'row'}>
-
-          <div className={'col'}>
-            <Brands />
-
-            <Purchase />
-          </div>
-
-          <div className={'col-md-4'}>
-            <GroupPurchasesPanel />
-          </div>
-
-        </div>
+        <Purchase />
       </div>
     </>
   );
