6019c2984e03820e3a24384874eebf1bfd217600
Verner Dsouza <verner.dz@gmail.com>
2022-07-20 20:36:23 -0400

    update state onClick

--- src/components/Purchase.js
+++ src/components/Purchase.js
@@ -1,10 +1,34 @@
+import React, {useState} from 'react'
 import {furnitureBrands} from '../seed'
 import Brand from './Brand'
 import GroupPurchasesPanel from './GroupPurchasesPanel'
 
 
-
 function Purchase() {
+  const [currentPurchases, setCurrentPurchases] = useState([])
+
+  const handlePurchase = (brand) => {
+
+    let currentPurchaseIds = currentPurchases.map(currentPurchase => currentPurchase.id)
+
+    if (currentPurchaseIds.includes(brand.id)) {
+
+      let modifiedPurchases = currentPurchases
+
+      modifiedPurchases = modifiedPurchases.filter(currentPurchase => {
+        return currentPurchase.id !== brand.id
+      })
+
+      setCurrentPurchases(modifiedPurchases)
+
+    } else {
+      let modifiedPurchases = currentPurchases
+      modifiedPurchases.push(brand)
+      setCurrentPurchases(modifiedPurchases)
+
+    }
+  }
+
   return(
     <>
       <div className={'row'}>
@@ -15,7 +39,7 @@ function Purchase() {
               {furnitureBrands.map((brand) => {
 
                 return (
-                  <div className={'col'} key={brand.id}>
+                  <div className={'col'} key={brand.id} onClick={() => handlePurchase(brand)}>
                     <Brand name={ brand.name} />
                   </div>
                 )
